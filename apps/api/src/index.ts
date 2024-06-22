e360b25558dc239f17f42246b6a72c73b63e0d16
Enzo Vieira <enzovieira16@hotmail.com>
2024-06-22 15:04:38 -0300

    project setup

--- apps/api/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { defineAbilityFor, projectSchema } from '@repo/auth'
-
-const ability = defineAbilityFor({ role: 'MEMBER', id: 'user-id' })
-
-const project = projectSchema.parse({ id: 'project-id', ownerId: 'user-2id' })
-
-console.log(ability.can('get', 'Billing'))
-console.log(ability.can('create', 'Invite'))
-console.log(ability.can('delete', project))
