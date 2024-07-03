4daae68482f4ffb67ee662566aec3aa5f8733430
Enzo Vieira <enzovieira16@hotmail.com>
2024-07-03 14:22:38 -0300

    add(web): theme switcher

--- apps/web/components/header.tsx
+++ apps/web/components/header.tsx
@@ -5,13 +5,19 @@ import { Slash } from 'lucide-react'
 import rocketseatIcon from '@/assets/rocketseat-icon.svg'
 import { OrganizationSwitcher } from './organization-switcher'
 import { ability } from '@/authenticate/auth'
+import { Separator } from './ui/separator'
+import { ThemeSwitcher } from './theme/theme-switcher'
 
 export async function Header() {
   const permissions = await ability()
   return (
     <div className="mx-auto flex max-w-[1200px] items-center justify-between">
       <div className="flex items-center gap-3">
-        <Image src={rocketseatIcon} alt="Rocketseat" className="size-6" />
+        <Image
+          src={rocketseatIcon}
+          alt="Rocketseat"
+          className="size-6 invert dark:invert-0"
+        />
 
         <Slash className="size-3 -rotate-[24deg] text-border" />
 
@@ -21,6 +27,8 @@ export async function Header() {
       </div>
 
       <div className="flex items-center gap-4">
+        <ThemeSwitcher />
+        <Separator orientation="vertical" className="h-5" />
         <ProfileButton />
       </div>
     </div>
